3750fb6dc6c3393e81f6ccd08838259c029ffaa4
Joe Amar <joseph.amar1391@gmail.com>
2019-10-22 20:24:27 -0500

    uncommented working code

--- rgb_w_servo1_servo2_usens/rgb_w_servo1_servo2_usens.ino
+++ rgb_w_servo1_servo2_usens/rgb_w_servo1_servo2_usens.ino
@@ -4,13 +4,6 @@
 #include <IRremote.h>
 #include <IRremoteInt.h>
 
-/* Example code for the Adafruit TCS34725 breakout library */
-
-/* Connect SCL    to analog 5
-   Connect SDA    to analog 4
-   Connect VDD    to 3.3V DC
-   Connect GROUND to common ground */
-
 /* Initialise with default values (int time = 2.4ms, gain = 1x) */
 Adafruit_TCS34725 tcs = Adafruit_TCS34725();
 Servo myservo;
@@ -36,32 +29,32 @@ void setup(void) {
   delay(2000);
 
 void loop(void) {
-//  uint16_t r, g, b, c, colorTemp, lux;
-//
-//  carriage.write(45);
-//
-//  for (int i = 0; i <= 3; i++){
-//      tcs.getRawData(&r, &g, &b, &c);
-//      // colorTemp = tcs.calculateColorTemperature(r, g, b);
-//      colorTemp = tcs.calculateColorTemperature_dn40(r, g, b, c);
-//      lux = tcs.calculateLux(r, g, b);
-//
-//      if ((colorTemp < 3500) && (c > 50)) {
-//        myservo.write(0);
-//        }
-//        
-//      else if ((colorTemp > 4300) && (colorTemp < 5300) && (c > 50)) {
-//        myservo.write(90);
-//        }
-//        
-//      else if ((colorTemp > 6800) && (colorTemp < 9100) && (c > 50)) {
-//        myservo.write(178);
-//        }
-//        
-//      delay(50);
-//    };
-//  delay(((avtime/2)-200));
-//  carriage.write(135);
-//  delay((avtime/2));
+  uint16_t r, g, b, c, colorTemp, lux;
+
+  carriage.write(45);
+
+  for (int i = 0; i <= 3; i++){
+      tcs.getRawData(&r, &g, &b, &c);
+      // colorTemp = tcs.calculateColorTemperature(r, g, b);
+      colorTemp = tcs.calculateColorTemperature_dn40(r, g, b, c);
+      lux = tcs.calculateLux(r, g, b);
+
+      if ((colorTemp < 3500) && (c > 50)) {
+        myservo.write(0);
+        }
+        
+      else if ((colorTemp > 4300) && (colorTemp < 5300) && (c > 50)) {
+        myservo.write(90);
+        }
+        
+      else if ((colorTemp > 6800) && (colorTemp < 9100) && (c > 50)) {
+        myservo.write(178);
+        }
+        
+      delay(50);
+    };
+  delay(((avtime/2)-200));
+  carriage.write(135);
+  delay((avtime/2));
 
 }
