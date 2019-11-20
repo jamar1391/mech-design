1caab6a1195e3000de4de1d10c1fa5cb43602a71
Joe Amar <joseph.amar1391@gmail.com>
2019-11-20 08:24:57 -0600

    final code

--- rgb_w_servo1_servo2_usens/rgb_w_servo1_servo2_usens.ino
+++ rgb_w_servo1_servo2_usens/rgb_w_servo1_servo2_usens.ino
@@ -10,6 +10,7 @@ Servo myservo;
 Servo carriage;
 int dly = 0;        // A variable to store the delay chosen in setup
 int RECV_PIN = 8;          //  The digital pin that the signal pin of the sensor is connected to
+int funnel = 155;          // The funnel position will be assigned to a variable which can later be used to change its position
 IRrecv receiver(RECV_PIN);  //  Create a new receiver object that would decode signals to key codes
 decode_results results;  
 
@@ -36,22 +37,22 @@ void setup(void) {
         }
       if (results.value==0xFF52AD) {
         Serial.println("Number 9");
-        dly = 100;
+//        dly = 100;
         break;
         }
       if (results.value==0xFF6897) {
         Serial.println("Number 0");
-        dly = 200;
+        dly = 100;
         break;
         }
       if (results.value==0xFF30CF) {
         Serial.println("Number 1");
-        dly = 300;
+        dly = 200;
         break;
         }
       if (results.value==0xFF18E7) {
         Serial.println("Number 2");
-        dly = 400;
+        dly = 300;
         break;
         }
         
@@ -84,15 +85,18 @@ void loop(void) {
       lux = tcs.calculateLux(r, g, b);
 
       if ((colorTemp < 3500) && (c > 35)) {
-        myservo.write(185);
+//        myservo.write(185);
+          funnel = 185;
         }
         
       else if ((colorTemp > 4300) && (colorTemp < 6000) && (c > 35)) {
-        myservo.write(155);
+//        myservo.write(155);
+          funnel = 155;
         }
         
       else if ((colorTemp > 6800) && (colorTemp < 9100) && (c > 35)) {
-        myservo.write(120);
+//        myservo.write(115);
+          funnel = 115;
         }
 
 //      Serial.print("Color Temp: ");Serial.print(colorTemp,DEC);
@@ -104,6 +108,7 @@ void loop(void) {
   delay(400);
   delay(dly);
   carriage.write(110);
+  myservo.write(funnel);
   delay(300);
 
 }
